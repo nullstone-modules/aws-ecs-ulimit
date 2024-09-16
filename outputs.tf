96981e4f7a13b277bfa31f3280c3c0ab8e65e697
Brad Sickles <brad.sickles@gmail.com>
2024-09-16 09:17:46 -0400

    Fixed names of softLimit, hardLimit in outputs

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,8 @@ output "ulimits" {
   value = [
     {
       name       = var.name
-      soft_limit = var.soft_limit
-      hard_limit = var.hard_limit
+      softLimit = var.soft_limit
+      hardLimit = var.hard_limit
     }
   ]
 }
